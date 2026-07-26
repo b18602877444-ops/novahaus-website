0b2ef22d494cea3c3dc38688ec10486f882a7e73
NOVAHAUS <hello@novahaus.studio>
2026-07-27 05:12:57 +0800

    fix: use correct NOVAHAUS horizontal logo

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -1,7 +1,8 @@
-import logoBlack from '../../brand/final/Logo_Black.png'
+import logoHorizontal from '../../brand/final/Horizontal_Logo.svg'
+import logoHorizontalWhite from '../../brand/final/Horizontal_White.svg'
 
 function BrandLogo({ reversed = false, className = '' }) {
-  return <img src={logoBlack} alt="NOVAHAUS" className={`brand-logo-asset ${className}`} />
+  return <img src={reversed ? logoHorizontalWhite : logoHorizontal} alt="NOVAHAUS" className={`brand-logo-asset ${className}`} />
 }
 
 export default BrandLogo
